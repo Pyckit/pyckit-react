f41cf0319337f6e29c83b3a4507a9db76467b0d6
Derek <derek@pyckit.com>
2025-07-27 15:30:22 -0600

    updated itemprocessimage

--- src/App.js
+++ src/App.js
@@ -240,7 +240,7 @@ const ItemCard = ({ item, index, onEdit, onRemove }) => {
       
       <div className="item-image-container">
         <img 
-          src={item.processedImage || item.stagedImage || 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22100%22%20height%3D%22100%22%20viewBox%3D%220%200%20100%20100%22%3E%3Crect%20width%3D%22100%22%20height%3D%22100%22%20fill%3D%22%23f0f0f0%22%2F%3E%3Ctext%20x%3D%2250%22%20y%3D%2250%22%20text-anchor%3D%22middle%22%20dy%3D%22.3em%22%20fill%3D%22%23999%22%20font-family%3D%22sans-serif%22%20font-size%3D%2214%22%3ENo%20Image%3C%2Ftext%3E%3C%2Fsvg%3E'}
+          src={item.thumbnail || item.image || item.processedImage || item.stagedImage || 'data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22100%22%20height%3D%22100%22%20viewBox%3D%220%200%20100%20100%22%3E%3Crect%20width%3D%22100%22%20height%3D%22100%22%20fill%3D%22%23f0f0f0%22%2F%3E%3Ctext%20x%3D%2250%22%20y%3D%2250%22%20text-anchor%3D%22middle%22%20dy%3D%22.3em%22%20fill%3D%22%23999%22%20font-family%3D%22sans-serif%22%20font-size%3D%2214%22%3ENo%20Image%3C%2Ftext%3E%3C%2Fsvg%3E'}
           alt={item.name || 'Detected item'}
           style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
         />
